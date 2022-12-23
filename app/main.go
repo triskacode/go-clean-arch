1dea1f1413f3661eb2a9882e623be59441ea408c
Triska Mahfud K <triska.triskacode@gmail.com>
2022-12-23 21:52:02 +0700

    initial commit

---
+++ app/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(cors.New())
+	app.Use(recover.New())
+	app.Use(logger.New(logger.Config{
+		Format: "[${time}] ${status} - ${latency} ${method} ${path}\n",
+	}))
+
+	app.Get("/ping", func(c *fiber.Ctx) error {
+		return c.SendString("pong")
+	})
+
+	log.Fatal(app.Listen(":9000"))
+}
